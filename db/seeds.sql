2de3a9f9f65bfb1bf37ca7549959f6a9d626910d
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-09-27 14:30:58 -0500

    created seeds.sql and ran in MySQL

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bruschetta Bout It Burger", true);
+INSERT INTO burgers (burger_name) VALUES ("Don't You Four Cheddar Bout Me Burger");
+INSERT INTO burgers (burger_name) VALUES ("Do The Brussel Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("New Bacon-ings Burger", true);
+
