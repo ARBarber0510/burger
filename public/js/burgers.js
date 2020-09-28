c62c9ef4659cd5ca043c284019c46d1c6cdc54a8
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-09-28 16:00:29 -0500

    added styling and modified burger.js

--- public/js/burgers.js
+++ public/js/burgers.js
@@ -1,22 +1,22 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    $(".change-sleep").on("click", function(event) {
+    $(".change-devour").on("click", function(event) {
       var id = $(this).data("id");
-      var newSleep = $(this).data("newsleep");
+      var newdevour = $(this).data("newdevour");
   
-      var newSleepState = {
-        sleepy: newSleep
+      var newDevState = {
+        devoured: newDev
       };
   
       // Send the PUT request.
-      $.ajax("/api/cats/" + id, {
+      $.ajax("/api/burgers/" + id, {
         type: "PUT",
-        data: newSleepState
+        data: newDevState
       }).then(
         function() {
-          console.log("changed sleep to", newSleep);
+          console.log("changed devour to", newdevour);
           // Reload the page to get the updated list
-          location.reload();
+          loburgerion.reload();
         }
       );
     });
@@ -25,35 +25,35 @@ $(function() {
       // Make sure to preventDefault on a submit event.
       event.preventDefault();
   
-      var newCat = {
+      var newburger = {
         name: $("#ca").val().trim(),
-        sleepy: $("[name=sleepy]:checked").val().trim()
+        devoured: $("[name=devoured]:checked").val().trim()
       };
   
       // Send the POST request.
-      $.ajax("/api/cats", {
+      $.ajax("/api/burgers", {
         type: "POST",
-        data: newCat
+        data: newburger
       }).then(
         function() {
-          console.log("created new cat");
+          console.log("created new burger");
           // Reload the page to get the updated list
-          location.reload();
+          loburgerion.reload();
         }
       );
     });
   
-    $(".delete-cat").on("click", function(event) {
+    $(".delete-burger").on("click", function(event) {
       var id = $(this).data("id");
   
       // Send the DELETE request.
-      $.ajax("/api/cats/" + id, {
+      $.ajax("/api/burgers/" + id, {
         type: "DELETE"
       }).then(
         function() {
-          console.log("deleted cat", id);
+          console.log("deleted burger", id);
           // Reload the page to get the updated list
-          location.reload();
+          loburgerion.reload();
         }
       );
     });
