2178bdbed4fdaa0fa4532e9a4abcc5e06073d92b
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-10-04 14:49:01 -0500

    modified css styling

--- public/js/burgers.js
+++ public/js/burgers.js
@@ -1,5 +1,27 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $("#bu").val().trim(),
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("Created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+
 
     $(".change-devoured").on("click", function(event) {
       var id = $(this).data("id");
@@ -24,27 +46,6 @@ $(function() {
       }).then(
         function() {
           console.log("Changed devour to", newDevour);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
-  
-    $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
-  
-      var newBurger = {
-        burger_name: $("#bu").val().trim(),
-      };
-  
-      // Send the POST request.
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          console.log("Created new burger");
           // Reload the page to get the updated list
           location.reload();
         }
