93c21ba640fedf976b97ba683b52f882c8874db9
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-10-04 13:51:33 -0500

    small code adjustments

--- config/orm.js
+++ config/orm.js
@@ -23,8 +23,7 @@ function printQuestionMarks(num) {
         if (typeof value === "string" && value.indexOf(" ") >= 0) {
           value = "'" + value + "'";
         }
-        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-        // e.g. {sleepy: true} => ["sleepy=true"]
+
         arr.push(key + "=" + value);
       }
     }
@@ -35,7 +34,7 @@ function printQuestionMarks(num) {
 
 // Object for all our SQL statement functions.
 var orm = {
-    all: function(tableInput, cb) {
+    selectAll: function(tableInput, cb) {
       var queryString = "SELECT * FROM " + tableInput + ";";
       connection.query(queryString, function(err, result) {
         if (err) {
@@ -44,7 +43,7 @@ var orm = {
         cb(result);
       });
     },
-    create: function(table, cols, vals, cb) {
+    insertOne: function(table, cols, vals, cb) {
       var queryString = "INSERT INTO " + table;
   
       queryString += " (";
@@ -65,7 +64,7 @@ var orm = {
       });
     },
     // An example of objColVals would be {name: panther, sleepy: true}
-    update: function(table, objColVals, condition, cb) {
+    updateOne: function(table, objColVals, condition, cb) {
       var queryString = "UPDATE " + table;
   
       queryString += " SET ";
